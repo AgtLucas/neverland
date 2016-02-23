394cd2d763d586afe203038da0be9040fa8ead62
Lucas <lucas@agtlucas.com>
2016-02-23 10:53:30 -0300

    Update index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -9,28 +9,66 @@ import React, {
   Component,
   StyleSheet,
   Text,
+  StatusBarIOS,
   View
 } from 'react-native'
 
 import Mapbox from 'react-native-mapbox-gl'
+import reactMixin from 'react-mixin'
 import secrets from './secrets'
 
 var mapRef = 'mapRef'
 
 class Neverland extends Component {
+  constructor (props) {
+    super(props)
+    return {
+      center: {
+        latitude: -26.4537736,
+        longitude: -49.1179425
+      },
+      zoom: 15
+    }
+  }
+
+  onRegionChange (location) {
+    this.setState({ currentZoom: location.zoom })
+  }
+
+  onRegionWillChange (location) {
+    console.log(location)
+  }
+
+  onUpdateUserLocation (location) {
+    console.log(location)
+  }
+
+  onLongPress (location) {
+    console.log('Long pressed', location)
+  }
+
   render () {
+    StatusBarIOS.setHidden(true)
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Mapbox
+          style={styles.map}
+          direction={0}
+          rotateEnabled={true}
+          scrollEnabled={true}
+          zoomEnabled={true}
+          showsUserLocation={true}
+          ref={mapRef}
+          accessToken={secrets.development.mapboxToken}
+          styleURL={this.mapStyles.emerald}
+          userTrackingMode={this.userTrackingMode.none}
+          centerCoordinate={this.state.center}
+          zoomLevel={this.state.zoom}
+          onRegionChange={this.onRegionChange}
+          onRegionWillChange={this.onRegionWillChange}
+          onUpdateUserLocation={this.onUpdateUserLocation}
+          onLongPress={this.onLongPress}
+           />
       </View>
     )
   }
@@ -38,21 +76,13 @@ class Neverland extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    flex: 1
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
+  map: {
+    flex: 1
   }
 })
+
+reactMixin(Neverland.prototype, Mapbox.mixin)
 
 AppRegistry.registerComponent('Neverland', () => Neverland)
